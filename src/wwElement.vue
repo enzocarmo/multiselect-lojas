73bbfc8664108c40a4845fcd6036b1ac2b0b5052
Enzo Carmo <enzocarmo@MacBook-Air-de-Enzo.local>
2024-07-18 17:47:31 -0300

    Commit

---
+++ src/wwElement.vue
@@ -0,0 +1,28 @@
+<template>
+  <Multiselect
+    v-model="selected"
+    :options="options"
+    mode="multiple"
+    :close-on-select="false"
+  />
+</template>
+
+<script>
+import Multiselect from "@vueform/multiselect";
+
+export default {
+  components: { Multiselect },
+  data() {
+    return {
+      selected: [],
+      options: [
+        { label: "Option 1", value: 1 },
+        { label: "Option 2", value: 2 },
+        { label: "Option 3", value: 3 },
+      ],
+    };
+  },
+};
+</script>
+
+<style src="@vueform/multiselect/themes/default.css"></style>
