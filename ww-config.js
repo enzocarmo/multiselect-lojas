e58299678fc9d5824fcffe0ae389f7e535cb71af
Enzo Carmo <78753090+enzocarmo@users.noreply.github.com>
2024-08-14 14:44:16 -0300

    Update ww-config.js

--- ww-config.js
+++ ww-config.js
@@ -14,6 +14,22 @@ export default {
       bindable: true,
       section: "Settings"
     },
+     required: {
+            type: 'OnOff',
+            label: {
+                en: 'Required',
+                fr: 'Required',
+            },
+            defaultValue: false,
+            bindable: true,
+            section: 'settings',
+            /* wwEditor:start */
+            bindingValidation: {
+                type: 'boolean',
+                tooltip: 'A boolean that defines if the input is required: `true | false`',
+            },
+            /* wwEditor:end */
+        }
   },
   triggerEvents: [
     { name: 'change', label: { en: 'Change' }, event: { value: '' } },
